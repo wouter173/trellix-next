9fb0f43ce2889ddcebd7fb8c0366f259c1ae7582
wouter <wouter@debruijn.dev>
2024-10-01 01:29:57 +0200

    chore: updated redirect uri

--- src/lib/auth/providers/github.ts
+++ src/lib/auth/providers/github.ts
@@ -1,4 +1,9 @@
 import { env } from '@/env'
 import { GitHub } from 'arctic'
 
-export const github = new GitHub(env.GITHUB_CLIENT_ID, env.GITHUB_CLIENT_SECRET)
+export const github = new GitHub(env.GITHUB_CLIENT_ID, env.GITHUB_CLIENT_SECRET, {
+  redirectURI:
+    env.NODE_ENV === 'production'
+      ? 'https://trellix-next.vercel.app/api/auth/callback/github'
+      : 'http://localhost:3000/api/auth/callback/github',
+})
