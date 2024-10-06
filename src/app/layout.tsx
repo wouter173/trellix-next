c918064e775e5327ae2f0dfcd3db4e187ed83516
wouter <wouter@debruijn.dev>
2024-10-06 20:46:05 +0200

    feat: add plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
+import Script from 'next/script'
 import { Toaster } from 'sonner'
 
 export const metadata: Metadata = {
@@ -14,6 +15,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
+      <Script
+        defer
+        id="plausible"
+        data-domain="trellix-next.vercel.app"
+        src="https://plausible.wouter.cloud/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"
+      />
+      <Script
+        id="inline-plausible"
+        dangerouslySetInnerHTML={{
+          __html: `window.plausible = window.plausible || function() {(window.plausible.q = window.plausible.q || []).push(arguments)}`,
+        }}
+      />
       <body className={`${GeistSans.variable} ${GeistMono.variable} relative min-h-screen bg-[#ececed] font-sans`}>
         <div className="absolute inset-0 -z-10 bg-[url(/dot.svg)] bg-repeat opacity-50"></div>
         <Toaster />
